f02645cc48302401f29600a3ccb94a31ec711309
Saeeda <saeedabatool39@gmail.com>
2025-09-11 13:24:30 -0400

    test ansible

--- aws/scripts/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-# Name of your container (matches your SSH script)
-CONTAINER_NAME="myapp"
-
-# Stop and remove if exists
-docker rm -f "$CONTAINER_NAME" || true
--- aws/scripts/validate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-curl -fsS http://localhost:80/ >/dev/null
